69387e604ca316ad703f9627a7b446133ed43807
howie1329 <46204910+howie1329@users.noreply.github.com>
2022-11-27 03:09:00 -0500

    lint commit

--- src/components/Destinationpage.js
+++ src/components/Destinationpage.js
@@ -1,19 +1,21 @@
 import React from "react";
 import Header from "./Header";
 
-function Cards(){
-    return(
+function Cards() {
+  return (
     <div>
-        <h2>01 Pick your destination</h2>
-    </div>)
+      <h2>01 Pick your destination</h2>
+    </div>
+  );
 }
 
-function Destinationpage(){
-    return(
+function Destinationpage() {
+  return (
     <div>
-        <Header />
-        <Cards />
-    </div>)
+      <Header />
+      <Cards />
+    </div>
+  );
 }
 
 export default Destinationpage;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,19 @@
 import React from "react";
-import Header from "./Header"
-import './Home.css'
+import Header from "./Header";
+import "./Home.css";
 import Homebody from "./Homebody";
 
-function Home(){
-    return(
-        <div className = 'home-container' >
-            <div className = 'header'>
-                <Header />
-            </div>
-            <div className = 'home-body'>
-                <Homebody />
-            </div>
-        </div>
-    )
+function Home() {
+  return (
+    <div className="home-container">
+      <div className="header">
+        <Header />
+      </div>
+      <div className="home-body">
+        <Homebody />
+      </div>
+    </div>
+  );
 }
 
 export default Home;
